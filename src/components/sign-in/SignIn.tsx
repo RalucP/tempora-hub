866819f16cfbfa187efa489eb3c648ab9a8d3e7e
Raluca Pârvan <parvanraluca0@gmail.com>
2024-03-21 16:14:33 +0100

    Add sign in form

---
+++ src/components/sign-in/SignIn.tsx
@@ -0,0 +1,48 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+
+import Button, { BUTTON_STYLE_TYPE } from '../button/Button';
+import Divider from '../divider/Divider';
+import FormInput from '../form-input/FormInput';
+
+import './SignIn.styles.scss';
+import { FC } from 'react';
+
+export type SignInProps = {
+  onToggleForm: () => void
+}
+
+const SingIn: FC<SignInProps> = ({ onToggleForm }) => {
+  return (
+    <section className={`sign-in-container form`}>
+      <h1>Sign In</h1>
+      <p>Sign in to see and manage your tasks</p>
+      <form>
+        <FormInput 
+          label='E-mail'
+          type='email'
+          placeholder=''
+          required
+        />
+        <FormInput 
+          label='Password'
+          type='password'
+          placeholder=''
+          required
+        />
+        <Button>Log in</Button>
+      </form>
+      <Divider>Or sign in with</Divider>
+      <Button buttonType={BUTTON_STYLE_TYPE.secondary}>
+        <FontAwesomeIcon icon={faGoogle} />
+        Google Account
+      </Button>
+      <div className='auth-options-container'>
+        <p>Don't have an account?</p>
+        <Button buttonType={BUTTON_STYLE_TYPE.tertiary} onClick={onToggleForm}>Sign up</Button>
+      </div>
+    </section>
+  )
+}
+
+export default SingIn;
