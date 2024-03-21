49121796dea60cbaa499e12f02f3e2995fde9dac
Raluca Pârvan <parvanraluca0@gmail.com>
2024-03-21 16:15:22 +0100

    Add sign in form to auth page

--- src/routes/authentication/Authentication.tsx
+++ src/routes/authentication/Authentication.tsx
@@ -1,10 +1,21 @@
+import { useState } from 'react';
+
 import SignUp from '../../components/sign-up/SignUp';
+import SignIn from '../../components/sign-in/SignIn';
 import './Authentication.styles.scss';
 
 const Authentication = () => {
+  const [ isSignUp, setIsSignUp ] = useState(true);
+
+  const handleToggleForm = () => {
+    setIsSignUp(!isSignUp);
+  }
+
   return(
     <div className='auth-container'>
-      <SignUp />
+      {
+        isSignUp ? <SignUp onToggleForm={handleToggleForm} />: <SignIn onToggleForm={handleToggleForm} />
+      }
     </div>
   )
 }
