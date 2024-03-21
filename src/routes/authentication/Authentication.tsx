cbd7db88cbb853f5cb65906cc2fb5839bc797f5b
Raluca Pârvan <parvanraluca0@gmail.com>
2024-03-21 15:03:15 +0100

    Add authentication page

---
+++ src/routes/authentication/Authentication.tsx
@@ -0,0 +1,12 @@
+import SignUp from '../../components/sign-up/SignUp';
+import './Authentication.styles.scss';
+
+const Authentication = () => {
+  return(
+    <div className='auth-container'>
+      <SignUp />
+    </div>
+  )
+}
+
+export default Authentication;
