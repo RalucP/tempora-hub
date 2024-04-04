d358a47d2eb16ba828cc12d4424f9f1e3e2c4584
Raluca Pârvan <parvanraluca0@gmail.com>
2024-04-05 00:23:24 +0200

    Add navbar component

---
+++ src/components/nav-bar/NavBar.tsx
@@ -0,0 +1,12 @@
+import User from '../user/User';
+import './NavBar.styles.scss';
+
+const NavBar = () => {
+  return(
+    <nav className="nav-bar">
+      <User />
+    </nav>
+  )
+}
+
+export default NavBar;
