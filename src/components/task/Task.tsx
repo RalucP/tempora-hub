acd0cda22e06dd4cde936f435a89831ab167839b
Raluca Pârvan <parvanraluca0@gmail.com>
2024-04-05 01:11:23 +0200

    Add status handleing logic to task component

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,16 +1,28 @@
-import { FC, InputHTMLAttributes, LabelHTMLAttributes } from 'react'
+import { FC, InputHTMLAttributes, LabelHTMLAttributes, useState } from 'react'
 import './Task.styles.scss'
 
-export type TaskProps = {
-  status: boolean
-} & InputHTMLAttributes<HTMLInputElement> 
+export type TaskProps =  InputHTMLAttributes<HTMLInputElement> 
 & LabelHTMLAttributes<HTMLLabelElement>
 
-const Task: FC<TaskProps> = ({ status, id, children, ...otherProps }) => {
+const Task: FC<TaskProps> = ({ id, children, ...otherProps }) => {
+  const [ status, setStatus ] = useState(true);
+
+  const onCheckboxChange = () => {
+    setStatus(!status);
+  }
+
   return (
     <div className='task-container'>
-      <input type="checkbox" className='task-checkbox' id={id} {...otherProps} checked={status}/>
-      <label htmlFor={id} className='task-label'>{children}</label>
+      <input 
+        type="checkbox" 
+        className='task-checkbox' 
+        id={id} onChange={onCheckboxChange} 
+        checked={status}
+        {...otherProps} 
+      />
+      <label htmlFor={id} className='task-label'>
+        {children}
+      </label>
     </div>
   )
 }
