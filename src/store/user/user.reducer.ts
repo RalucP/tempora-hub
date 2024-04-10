6dd9ddf6257a80150bb7542dc0bb86166e3f0431
Raluca Pârvan <parvanraluca0@gmail.com>
2024-04-10 21:42:55 +0200

    Implement redux for useer

---
+++ src/store/user/user.reducer.ts
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type UserData = {
+  email: string,
+  displayName: string
+}
+
+export type UserState = {
+  readonly currentUser: UserData | null
+}
+
+const INITIAL_STATE: UserState = {
+  currentUser: null,
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: INITIAL_STATE,
+  reducers: {
+    setCurrentUser(state, action) {
+      state.currentUser = action.payload;
+    }
+  }
+});
+
+export const { setCurrentUser } = userSlice.actions;
+
+export const userReducer = userSlice.reducer;
