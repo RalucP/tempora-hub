1820e454c8b7fe95ccb3648680ffe4dc805dbeff
Raluca Pârvan <parvanraluca0@gmail.com>
2024-04-10 21:57:49 +0200

    Add redux-logger

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import logger from 'redux-logger';
 
 import { rootReducer } from "./rootReducer";
 
 export type RootState = ReturnType<typeof rootReducer>;
 
+const middlewares = !import.meta.env.PROD ? [logger] : [];
+
 export const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middlewares)
 });
