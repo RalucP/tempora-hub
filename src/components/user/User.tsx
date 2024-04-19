daaefd5a90b2ed329b7f41724dd78e3bbc3d8d57
Raluca Pârvan <parvanraluca0@gmail.com>
2024-04-19 15:28:37 +0200

    Add initials to user profile

--- src/components/user/User.tsx
+++ src/components/user/User.tsx
@@ -1,9 +1,29 @@
+import { useSelector } from 'react-redux';
+import { selectCurrentUser } from '../../store/user/user.selector';
+
 import './User.styles.scss';
+import { useEffect, useState } from 'react';
 
 const User = () => {
+  const [ initials, setInitials ] = useState('');
+
+  const currentUser = useSelector(selectCurrentUser);
+
+  useEffect(() => {
+    if(!currentUser) return;
+    const { displayName, email } = currentUser;
+
+    if(!displayName) {
+      setInitials(email.substring(0,1).toLocaleUpperCase());
+    }
+    else{
+      setInitials(displayName.substring(0, 1).toLocaleUpperCase());
+    }
+  }, [currentUser]);
+
   return (
     <div className='user-icon'>
-      RP
+      {initials}
     </div>
   )
 }
