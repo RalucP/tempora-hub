a61c3217f5b12e5e08214526e3c47697465e3698
Raluca Pârvan <parvanraluca0@gmail.com>
2024-05-07 00:02:26 +0200

    Sort tasks by status

--- src/store/tasks/tasks.reducer.ts
+++ src/store/tasks/tasks.reducer.ts
@@ -11,12 +11,12 @@ const INITIAL_STATE: TaskState = {
   tasks: []
 };
 
-const addNewTask = (tasks: TaskState, newTask: Task) => {
-  return [...tasks.tasks, newTask];
+const addNewTask = (tasks: Task[], newTask: Task) => {
+  return [newTask, ...tasks];
 }
 
-const setStatus = (tasks: TaskState, taskToBeUpdatedId: string) => {
-  return tasks.tasks.map((task) => {
+const setStatus = (tasks: Task[], taskToBeUpdatedId: string) => {
+  return tasks.map((task) => {
     let tempStatus = false;
 
     if(task.id === taskToBeUpdatedId){
@@ -28,12 +28,24 @@ const setStatus = (tasks: TaskState, taskToBeUpdatedId: string) => {
   })
 }
 
-const removeTask = (tasks: TaskState, taskToBeDeletedId: string) => {
-  const existingTask = tasks.tasks.find((task) => task.id === taskToBeDeletedId)
+const removeTask = (tasks: Task[], taskToBeDeletedId: string) => {
+  const existingTask = tasks.find((task) => task.id === taskToBeDeletedId)
 
-  if(!existingTask) return tasks.tasks;
+  if(!existingTask) return tasks;
 
-  return tasks.tasks.filter(task =>  task.id !== taskToBeDeletedId)
+  return tasks.filter(task =>  task.id !== taskToBeDeletedId)
+}
+
+const sortTasksByStatus = (tasks: Task[]) => {
+  return tasks.sort((taskA, taskB) => {
+    if (taskA.status === false && taskB.status === true) {
+      return -1; // taskA comes before taskB
+    } else if (taskA.status === true && taskB.status === false) {
+      return 1; // taskB comes before taskA
+    } else {
+      return 0; // Keep the order unchanged
+    }
+  })
 }
 
 export const updateTaskStatus = createAsyncThunk(
@@ -66,10 +78,10 @@ export const taskSlice = createSlice({
   initialState: INITIAL_STATE,
   reducers: {
     setTasks(state, action) {
-      state.tasks = action.payload;
+      state.tasks = sortTasksByStatus(action.payload)
     },
     addTask(state, action) {
-      state.tasks = addNewTask(state, action.payload)
+      state.tasks = sortTasksByStatus(addNewTask(state.tasks, action.payload))
     }
   },
   extraReducers: (builder) => {
@@ -79,14 +91,14 @@ export const taskSlice = createSlice({
 
         if(!taskId) return;
 
-        state.tasks = setStatus(state, taskId);
+        state.tasks = sortTasksByStatus(setStatus(state.tasks, taskId));
       })
       .addCase(deleteTask.fulfilled, (state, action) => {
         const taskId = action.payload;
 
         if(!taskId) return;
 
-        state.tasks = removeTask(state, taskId);
+        state.tasks = sortTasksByStatus(removeTask(state.tasks, taskId));
       })
   }
 });
